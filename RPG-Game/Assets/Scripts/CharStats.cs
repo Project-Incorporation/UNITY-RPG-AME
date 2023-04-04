22110b793be320b2d93832bb418856212b43aa31
j3k01 <kostek13319@gmail.com>
2023-04-04 22:21:44 +0200

    Added Levelling up system and stat system

---
+++ RPG-Game/Assets/Scripts/CharStats.cs
@@ -0,0 +1,83 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharStats : MonoBehaviour
+{
+
+    public string charName;
+    public int playerLevel = 1;
+    public int currentEXP;
+    public int[] expToNextLevel;
+    public int maxLevel = 100;
+    public int baseEXP = 1000;
+
+    public int currentHP;
+    public int maxHP = 100;
+    public int currentMP;
+    public int maxMP = 30;
+    public int[] mpLvlBonus;
+    public int strenght;
+    public int defence;
+    public int weaponPower;
+    public int armorPower;
+    public string equippedWeapon;
+    public string equippedArmor;
+    public Sprite charImage;
+
+
+    void Start()
+    {
+        expToNextLevel = new int[maxLevel];
+        expToNextLevel[1] = baseEXP;
+
+        for(int i =2; i < expToNextLevel.Length; i++)
+        {
+            expToNextLevel[i] = Mathf.FloorToInt(expToNextLevel[i - 1] * 1.05f);
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.K))
+        {
+            AddEXP(500);
+        }
+        
+    }
+    public void AddEXP(int expToAdd)
+    {
+        currentEXP += expToAdd;
+        if(playerLevel < maxLevel)
+        {
+            if(currentEXP > expToNextLevel[playerLevel]) 
+            {
+                currentEXP -= expToNextLevel[playerLevel];
+                playerLevel++;
+
+                if(playerLevel % 2 == 0)
+                {
+                    strenght++;
+                }
+                else
+                {
+                    defence++;
+                }
+
+                maxHP = Mathf.FloorToInt(maxHP * 1.05f);
+                currentHP = maxHP;
+
+                maxMP += mpLvlBonus[playerLevel];
+                currentMP = maxMP;
+            }
+        }
+
+        if (playerLevel >= maxLevel) 
+        {
+            currentEXP = 0;
+        }
+    }
+
+}

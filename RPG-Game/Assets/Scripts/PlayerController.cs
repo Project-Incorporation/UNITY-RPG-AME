86789bd27088e9ddd28f925f7b0e9cec2574ac7e
Rafał Żarczyński <41727826+Arithil@users.noreply.github.com>
2023-04-05 06:10:38 +0200

    Camera rework moving between scenes

--- RPG-Game/Assets/Scripts/PlayerController.cs
+++ RPG-Game/Assets/Scripts/PlayerController.cs
@@ -10,10 +10,24 @@ public class PlayerController : MonoBehaviour
 
     public Animator myAnim;
 
+    public static PlayerController instance;
+
+    public string areaTransitionName;
+    private Vector3 bottomLeftLimit;
+    private Vector3 topRightLimit;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if(instance == null)
+        {
+            instance = this;
+        } else
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(gameObject);
     }
 
     // Update is called once per frame
@@ -28,5 +42,13 @@ public class PlayerController : MonoBehaviour
             myAnim.SetFloat("LastMoveX", Input.GetAxisRaw("Horizontal"));
             myAnim.SetFloat("LastMoveY", Input.GetAxisRaw("Vertical")); 
         }
+
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, bottomLeftLimit.x, topRightLimit.x), Mathf.Clamp(transform.position.y, bottomLeftLimit.y, topRightLimit.y), transform.position.z);
+    }
+
+    public void SetBounds(Vector3 botLeft, Vector3 topRight)
+    {
+        bottomLeftLimit = botLeft + new Vector3(1f, 1f, 0f);
+        topRightLimit = topRight + new Vector3(-1f, -1f, 0f);
     }
 }

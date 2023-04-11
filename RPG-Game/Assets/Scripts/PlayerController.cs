f5857f0cbcf50152286c41f975043645fda1c2fc
Maroosek <126722704+Maroosek@users.noreply.github.com>
2023-04-11 18:08:49 +0200

    Dialogi full, Poszerzenie mapy

--- RPG-Game/Assets/Scripts/PlayerController.cs
+++ RPG-Game/Assets/Scripts/PlayerController.cs
@@ -16,6 +16,8 @@ public class PlayerController : MonoBehaviour
     private Vector3 bottomLeftLimit;
     private Vector3 topRightLimit;
 
+    public bool canMove = true;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -33,14 +35,25 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        theRB.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * moveSpeed;
+        if(canMove)
+        {
+            theRB.velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * moveSpeed;
+        }
+        else
+        {
+            theRB.velocity = Vector2.zero;
+        }
+        
         myAnim.SetFloat("MoveX",theRB.velocity.x);
         myAnim.SetFloat("MoveY",theRB.velocity.y);
 
         if(Input.GetAxisRaw("Horizontal") == 1 || Input.GetAxisRaw("Horizontal") == -1 || Input.GetAxisRaw("Vertical") == 1 || Input.GetAxisRaw("Vertical") == -1)
         {
-            myAnim.SetFloat("LastMoveX", Input.GetAxisRaw("Horizontal"));
-            myAnim.SetFloat("LastMoveY", Input.GetAxisRaw("Vertical")); 
+            if(canMove)
+            {
+                myAnim.SetFloat("LastMoveX", Input.GetAxisRaw("Horizontal"));
+                myAnim.SetFloat("LastMoveY", Input.GetAxisRaw("Vertical"));
+            }
         }
 
         transform.position = new Vector3(Mathf.Clamp(transform.position.x, bottomLeftLimit.x, topRightLimit.x), Mathf.Clamp(transform.position.y, bottomLeftLimit.y, topRightLimit.y), transform.position.z);

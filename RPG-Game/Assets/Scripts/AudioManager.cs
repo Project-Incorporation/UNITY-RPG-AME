b7d8e6b54ecdaa41b75ac0b891fb72a2ac1f2c0f
j3k01 <kostek13319@gmail.com>
2023-05-14 22:46:12 +0200

    Added music and it's awesome

---
+++ RPG-Game/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class AudioManager : MonoBehaviour
+{
+
+    public AudioSource[] sfx;
+    public AudioSource[] bgm;
+
+    public static AudioManager instance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        instance = this;
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        PlayRandomMusic();
+    }
+
+    public void PlaySFX(int soundToPlay)
+    {
+        if(soundToPlay < sfx.Length)
+        {
+            sfx[soundToPlay].Play();
+        }
+
+    }
+
+    public void PlayBGM(int musicToPlay)
+    {
+        StopMusic();
+
+        if(musicToPlay < bgm.Length)
+        {
+            bgm[musicToPlay].Play();
+        }
+    }
+
+    public void StopMusic()
+    {
+        for(int i = 0; i < bgm.Length;  i++) 
+        {
+            bgm[i].Stop();
+        }
+    }
+
+    public void PlayRandomMusic()
+    {
+        System.Random random = new System.Random();
+        int randomNumber = random.Next(1, bgm.Length);
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            PlayBGM(randomNumber);
+        }
+    }
+}
